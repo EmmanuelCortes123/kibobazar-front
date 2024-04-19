592748cd7e1b21678549836c8a99792c9fba29e4
crist <cristopherozzy17@gmail.com>
2024-04-19 15:52:34 -0600

    se agrego el carrito y favoritos

--- formulario/formularioActions.js
+++ formulario/formularioActions.js
@@ -1,15 +1,26 @@
 // Arreglo global que contendrá los productos
 let publicaciones = JSON.parse(localStorage.getItem("productos")) || [];
 let favorit = JSON.parse(localStorage.getItem("favorite")) || [];
+let carrit= JSON.parse(localStorage.getItem("carrito")) || [];
 
 function inicializarProductos() {
   renderizarProductos(); // Solo renderiza los productos existentes en Local Storage al inicializar
 }
 
+
+
+
 const updateFavoritesInLocalStorage = () => {
 	localStorage.setItem('favorites', JSON.stringify(favorites));
 };
 
+const updateCarritoInLocalStorage = () => {
+	localStorage.setItem('carrito', JSON.stringify(carritos));
+};
+
+
+
+//----------------- CREAR PRODUCTO DE PRODUCTOS
 function crearProducto(name, image, category, material, sizes, description, measurements, price) {
   let producto = { name, image, category, material, sizes, description, measurements, price };
   publicaciones.push(producto);
@@ -17,6 +28,10 @@ function crearProducto(name, image, category, material, sizes, description, meas
   renderizarProductos(); // Actualiza la vista cada vez que se crea un producto
 }
 
+
+
+
+//--------------- BORRAR PRODUCTO DE PRODUCTOS
 function borrarPublicacion(i) {
   if (i >= 0 && i < publicaciones.length) {
     publicaciones.splice(i, 1);
@@ -25,6 +40,9 @@ function borrarPublicacion(i) {
   }
 }
 
+
+
+//--------------------MODIFICAR PRODUCTO DE PRODUCTOS
 function modificarProducto(i, name, image, category, material, sizes, description, measurements, price) {
   if (i >= 0 && i < publicaciones.length) {
     publicaciones[i] = { name, image, category, material, sizes, description, measurements, price };
@@ -33,9 +51,16 @@ function modificarProducto(i, name, image, category, material, sizes, descriptio
   }
 }
 
+
+
+
  // <button onclick="agFavorito(${indice})" class="favorite">Me gusta</button>
 
 document.addEventListener("DOMContentLoaded", inicializarProductos);
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
   const contenedorProductos = document.getElementById('contenedorProductos');
@@ -75,23 +100,18 @@ document.addEventListener('DOMContentLoaded', () => {
 									<i
 										class="fa-regular fa-heart"
 										id="favorite-regular"
-										<abbr title="Elimiar de favoritos"  > MG</abbr>
-										
+										<abbr title="Elimiar de favoritos"  > MG</abbr>					
 									</i>
-									<i
-										class="fa-solid fa-heart"
-										id="added-favorite"
-									> favorito</i>
 								</button>
 
-								<button class="cart">
+								<button onclick="agCarrito(${indice})" class="cart">
 									<i class="fa-solid fa-bag-shopping" <abbr title="Agregar a la bolsa">B</abbr> </i>
 									
 								</button>
 							</div>
 
-				</div>
-            </div>
+				
+            
           </div>
         </div>`;
       
@@ -105,10 +125,14 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('favorite', JSON.stringify(favorit));
   };
 
+  const updateCarritoInLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(carrit));
+  };
+
 
  
   
-/////prueba 1 SE QUEDA
+//-------------------------------------------------------- PARTE DE FAVORITOS AGREGAR ----------------------------------
  const agregarFav=(favo)=>{
     const index= favorit.findIndex(
       element => element.name === favo.name
@@ -124,7 +148,6 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
   };
-///prueba 1 SE QUEDA
   window.agFavorito = (indice) => {
     let productos = JSON.parse(localStorage.getItem('productos')) || [];
     let palabra= productos[indice].nombre;
@@ -135,18 +158,38 @@ document.addEventListener('DOMContentLoaded', () => {
     renderizarProductos(); // Re-renderiza los productos después de borrar
   };
 
+//----------------------------------------------------------PARTE DE FAVORITOS AGREGAR --------------------------------------------
 
-///propuesta 2
-  window.agregarFavorito = (indice) => {
-    let productos = JSON.parse(localStorage.getItem('productos')) || [];
-    let palabra= productos[indice].nombre;
-    let favo={
-      name:palabra,
-    };
-    favorit.push(favo);
-    localStorage.setItem('favorite', JSON.stringify(favorit));
-    renderizarProductos(); // Re-renderiza los productos después de borrar
+
+//-------------------------------------------------------- PARTE DE CARRITO AGREGAR ----------------------------------
+const agregarCarr=(fav)=>{
+  const index= carrit.findIndex(
+    element => element.name === fav.name
+  );
+
+  console.log("el resultado de la prueba es"+index);
+  if (index > -1) {
+    carrit.splice(index, 1);
+    updateCarritoInLocalStorage();
+  } else {
+    carrit.push(fav);
+    updateCarritoInLocalStorage();
+  }
+
+};
+window.agCarrito = (indice) => {
+  let productos = JSON.parse(localStorage.getItem('productos')) || [];
+  let palabra= productos[indice].nombre;
+  let fav={
+    name:palabra,
   };
+  agregarCarr(fav);
+  renderizarProductos(); // Re-renderiza los productos después de borrar
+};
+
+//----------------------------------------------------------PARTE DE CARRITO AGREGAR --------------------------------------------
+
+
 
   //  el boton de borrar producto
   window.borrarProducto = (indice) => {
@@ -155,21 +198,6 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('productos', JSON.stringify(productos));
     renderizarProductos(); // Re-renderiza los productos después de borrar
   };
-
-//propuesta 2
- window.borrarFavorito = (indice) => {
-    let productos = JSON.parse(localStorage.getItem('favorite')) || [];
-    productos.splice(indice, 1);
-    localStorage.setItem('favorite', JSON.stringify(productos));
-
-    renderizarProductos(); // Re-renderiza los productos después de borrar
-  };
- 
-  
-
-
- 
-
 
   
   // Llama a renderizarProductos al cargar la página
