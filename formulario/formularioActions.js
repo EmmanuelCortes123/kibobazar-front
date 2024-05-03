7d8c700ee0c74a79ffdb9ef95d4b6d64627cdfca
alberto9508 <toledogallegosjosealberto@gmail.com>
2024-05-03 15:05:59 -0600

    se actualiza

--- formulario/formularioActions.js
+++ formulario/formularioActions.js
@@ -615,9 +615,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 <div class="d-flex align-items-center">
 
                   <button class="btnsNew fa-solid fa-envelope " onclick ="borrarProducto(${indice})" >
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 748 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 612"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                  <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                   <title>Eliminar Producto</title>
                   </svg>
+                  <i
+                  class="fa-solid fa-heart"
+                  id="added-favorite"
+                > favorito</i>
                   </button>
 
                   <button onclick="agFavorito(${indice})"  class="favorite">
@@ -632,9 +637,12 @@ document.addEventListener('DOMContentLoaded', () => {
                   </button>
 
                   <button onclick="agCarrito(${indice})" class="cart">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 748 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 612 612"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                  <path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/>
+                 
                   <title>Agregar al Carrito</title>
                   </svg>
+                  
                   </button>
                 </div>
 							</div>
