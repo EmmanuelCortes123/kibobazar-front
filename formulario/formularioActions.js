cf7f74e3e614feca23ff4e619f7f880576e3f92e
Joshua Navarro <joshuabarron711@gmail.com>
2024-05-03 13:37:03 -0600

    Merge con rama cristopher para la unificacion del proyecto

--- formulario/formularioActions.js
+++ formulario/formularioActions.js
@@ -602,22 +602,6 @@ document.addEventListener('DOMContentLoaded', () => {
     productos.forEach((producto, indice) => {
       const tarjetaProducto = `
         <div class="col-sm-4 mb-4">
-<<<<<<< HEAD
-          <div class="card">
-            <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-            <div class="card-body">
-              <h5 class="card-title text-center">${producto.nombre}</h5>
-              <hr>
-              <p class="card-text text-center">${producto.descripcion}</p>
-              <hr>
-              <ul class="list-unstyled mb-3">
-                <li><strong>Categoría:</strong> ${producto.categoria}</li>
-                <li><strong>Material:</strong> ${producto.material}</li>
-                <li><strong>Medidas:</strong> ${producto.medidas}</li>
-                <li><strong>Tallas:</strong> ${producto.tallas}</li>
-              <li><strong>Precio:</strong> ${producto.precio}</p>
-              </ul>
-=======
 
         
 
@@ -628,37 +612,18 @@ document.addEventListener('DOMContentLoaded', () => {
         </button>
         </a>
         <div class="card-body">
-          <h5 class="card-title">${producto.nombre}</h5>
-          <p class="card-text">${producto.descripcion}</p>
-          <p class="card-text">${producto.categoria}</p>
-          <p class="card-text">${producto.material}</p>
-          <p class="card-text">${producto.medidas}</p>
-          <p class="card-text">${producto.tallas}</p>
-          <p class="card-text">${producto.precio} MXN</p>
-
-              <div class="container-buttons-card">
-
-              <button class="btnsNew fa-solid fa-envelope" onclick ="borrarProducto(${indice})" >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 748 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
-                
-              </button>
-
-              <button onclick="agFavorito(${indice})"  class="favorite">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 748 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M225.8 468.2l-2.5-2.3L48.1 303.2C17.4 274.7 0 234.7 0 192.8v-3.3c0-70.4 50-130.8 119.2-144C158.6 37.9 198.9 47 231 69.6c9 6.4 17.4 13.8 25 22.3c4.2-4.8 8.7-9.2 13.5-13.3c3.7-3.2 7.5-6.2 11.5-9c0 0 0 0 0 0C313.1 47 353.4 37.9 392.8 45.4C462 58.6 512 119.1 512 189.5v3.3c0 41.9-17.4 81.9-48.1 110.4L288.7 465.9l-2.5 2.3c-8.2 7.6-19 11.9-30.2 11.9s-22-4.2-30.2-11.9zM239.1 145c-.4-.3-.7-.7-1-1.1l-17.8-20c0 0-.1-.1-.1-.1c0 0 0 0 0 0c-23.1-25.9-58-37.7-92-31.2C81.6 101.5 48 142.1 48 189.5v3.3c0 28.5 11.9 55.8 32.8 75.2L256 430.7 431.2 268c20.9-19.4 32.8-46.7 32.8-75.2v-3.3c0-47.3-33.6-88-80.1-96.9c-34-6.5-69 5.4-92 31.2c0 0 0 0-.1 .1s0 0-.1 .1l-17.8 20c-.3 .4-.7 .7-1 1.1c-4.5 4.5-10.6 7-16.9 7s-12.4-2.5-16.9-7z"/></svg>
-                <i
-                  class="fa-solid fa-heart"
-                  id="added-favorite"
-                > favorito</i>
-              </button>
-
-              <button onclick="agCarrito(${indice})" class="cart">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 748 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M160 112c0-35.3 28.7-64 64-64s64 28.7 64 64v48H160V112zm-48 48H48c-26.5 0-48 21.5-48 48V416c0 53 43 96 96 96H352c53 0 96-43 96-96V208c0-26.5-21.5-48-48-48H336V112C336 50.1 285.9 0 224 0S112 50.1 112 112v48zm24 48a24 24 0 1 1 0 48 24 24 0 1 1 0-48zm152 24a24 24 0 1 1 48 0 24 24 0 1 1 -48 0z"/></svg>
-                
-              </button>
->>>>>>> 39aa74f9ec8eb74b7d6d0520e1392c254d2419e7
-
-              
-
+          <h5 class="card-title text-center">${producto.nombre}</h5>
+            <hr>
+              <p class="card-text text-center">${producto.descripcion}</p>
+            <hr>
+            <ul class="list-unstyled mb-3">
+            <li><strong>Categoría:</strong> ${producto.categoria}</li>
+            <li><strong>Material:</strong> ${producto.material}</li>
+            <li><strong>Medidas:</strong> ${producto.medidas}</li>
+            <li><strong>Tallas:</strong> ${producto.tallas}</li>
+            <li><strong>Precio:</strong> ${producto.precio} MXN</p>
+            </ul>
+          
               <div class="container-buttons-card d-flex justify-content-between">
                 <div class="d-flex align-items-center">
 
